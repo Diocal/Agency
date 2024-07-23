1ee96d045207505f5871e42b048e839df9fbe9f4
diego calderon <diegocalderon9999@gmail.com>
2024-07-23 10:31:40 +0300

    Css

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site:"https://diocal.github.io/Agency",
+  site:"https://diocal.github.io",
   base: '/Agency/',
   integrations: [, ],
   output: 'static',
