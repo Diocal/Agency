2b206d187885771a43a9a0772b9658c6318d0e1d
diego calderon <diegocalderon9999@gmail.com>
2024-07-23 08:44:33 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://diocal.github.io",
   integrations: [, ],
   output: 'server',
   server: {
