eec816141f4d2787982a0274cb1fbf07bccc79a8
diego calderon <diegocalderon9999@gmail.com>
2024-07-23 10:20:59 +0300

    Configure Astro for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site:"https://diocal.github.io/Agency",
-  base: '/Agency/',
+
   integrations: [, ],
   output: 'static',
   server: {
