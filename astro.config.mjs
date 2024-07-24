7bb83243c9c45d597c9f579e0c12734802be58df
diego calderon <diegocalderon9999@gmail.com>
2024-07-24 08:44:36 +0300

    add images

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site:"https://diocal.github.io",
-  base: '/Agency/',
+
+
   integrations: [, ],
   output: 'static',
   outDir:'dist',
